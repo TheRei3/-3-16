788733581fc2edd5a9ca863c03657d55b0b9bfc5
admin <admin@DESKTOP-8L2IG92>
2021-04-23 06:23:35 +0900

    ex3no16

---
+++ ex3no16/src/ex3no16/ex3no16.java
@@ -0,0 +1,10 @@
+package ex3no16;
+
+public class ex3no16 {  //105p
+	public static void main(String[] args) {
+		float pi = 3.141592f;
+		float shortPi = (int)(pi * 1000) / 1000f;
+		System.out.println(shortPi);
+	}
+
+}
